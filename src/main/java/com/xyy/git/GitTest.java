1fd86e15dab8a03a6dc950f033750cf212ac5bfb
zimo <junmuzimo@gmail.com>
2022-01-03 17:16:50 +0800

    first commit

---
+++ src/main/java/com/xyy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xyy.git;
+
+/**
+ * @author zimo
+ * @date 2022-01-03 17:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
